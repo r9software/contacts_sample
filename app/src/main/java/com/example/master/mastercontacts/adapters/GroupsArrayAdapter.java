895094e775f6a7345161e52ff2880fa858834358
Rodolfo Abarca <master@MacBook-Pro-de-Rodolfo.local>
2017-03-30 23:25:25 -0600

    added groups list view and small design changes

---
+++ app/src/main/java/com/example/master/mastercontacts/adapters/GroupsArrayAdapter.java
@@ -0,0 +1,44 @@
+package com.example.master.mastercontacts.adapters;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.master.mastercontacts.R;
+import com.example.master.mastercontacts.model.Group;
+
+import java.util.List;
+
+/**
+ * Created by master on 30/03/17.
+ */
+
+public class GroupsArrayAdapter extends ArrayAdapter<Group> {
+
+
+    public GroupsArrayAdapter(@NonNull Context context, @NonNull List<Group> objects) {
+        super(context, 0, objects);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Group user = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.group_item, parent, false);
+        }
+        // Lookup view for data population
+        TextView textViewName = (TextView) convertView.findViewById(R.id.nameTextView);
+        TextView textViewDescription = (TextView) convertView.findViewById(R.id.descriptionTextView);
+        // Populate the data into the template view using the data object
+        textViewName.setText(user.getName());
+        textViewDescription.setText(user.getDescription());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/master/mastercontacts/fragments/GroupsFragment.java
+++ app/src/main/java/com/example/master/mastercontacts/fragments/GroupsFragment.java
@@ -1,19 +1,32 @@
 package com.example.master.mastercontacts.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.master.mastercontacts.GroupDetailActivity;
 import com.example.master.mastercontacts.R;
+import com.example.master.mastercontacts.adapters.GroupsArrayAdapter;
+import com.example.master.mastercontacts.model.Contact;
+import com.example.master.mastercontacts.model.Group;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GroupsFragment extends Fragment {
 
+
+    private ArrayList<Group> groups;
 
     public GroupsFragment() {
         // Required empty public constructor
@@ -24,7 +37,28 @@ public class GroupsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+        View view= inflater.inflate(R.layout.fragment_groups, container, false);
+        ListView mList=(ListView)view.findViewById(R.id.group_list_view);
+        groups = new ArrayList<>();
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent mIntent = new Intent(getContext(), GroupDetailActivity.class);
+                mIntent.putExtra(GroupDetailActivity.GROUP_ID, groups.get(position).getId());
+                startActivity(mIntent);
+            }
+        });
+
+        for (int x = 0; x < 10; x++) {
+            Group group = new Group();
+            group.setId(x);
+            group.setName("Group " + x);
+            group.setDescription("111-222-333-" + x);
+            groups.add(group);
+        }
+        ArrayAdapter<Group> mAdapter= new GroupsArrayAdapter(getContext(),groups);
+        mList.setAdapter(mAdapter);
+        return view;
     }
 
 }
