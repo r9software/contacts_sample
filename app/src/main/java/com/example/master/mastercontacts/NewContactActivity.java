f125ab5d10273df728b488210cd25d2d8cd5846c
Rodolfo Abarca <master@MacBook-Pro-de-Rodolfo.local>
2017-03-31 01:58:45 -0600

    Contact add screen

--- app/src/main/java/com/example/master/mastercontacts/NewContactActivity.java
+++ app/src/main/java/com/example/master/mastercontacts/NewContactActivity.java
@@ -2,6 +2,7 @@ package com.example.master.mastercontacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.master.mastercontacts.model.Contact;
@@ -10,13 +11,18 @@ public class NewContactActivity extends AppCompatActivity {
 
     public static final String CONTACT = "contact";
     public static final String PHONE = "phone";
-    private TextView mtext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
-        mtext = (TextView) findViewById(R.id.nameTextView);
+        findViewById(R.id.showMoreButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMoreViews();
+            }
+        });
         if (getIntent().getExtras() != null) {
             Contact contact = (Contact) getIntent().getExtras().getSerializable(CONTACT);
             if (contact != null) {
@@ -25,7 +31,25 @@ public class NewContactActivity extends AppCompatActivity {
         }
     }
 
+    private void showMoreViews() {
+        findViewById(R.id.NickNameEditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.corporationEditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.titleEditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.phone2EditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.phone3EditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.type2EditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.type3EditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.addressEditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.werbsiteEditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.groupEditText).setVisibility(View.VISIBLE);
+        findViewById(R.id.notesEditText).setVisibility(View.VISIBLE);
+
+
+
+
+    }
+
     private void fillFields(Contact contact) {
-        mtext.setText(contact.getName());
+
     }
 }
